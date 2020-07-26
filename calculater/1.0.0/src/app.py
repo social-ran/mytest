ec53a68bdcca646351dbc4a1c7cdab1f287d3fdc
social-ran <68770158+social-ran@users.noreply.github.com>
2020-07-26 18:57:09 +0800

    Update app.py

--- calculater/1.0.0/src/app.py
+++ calculater/1.0.0/src/app.py
@@ -1,23 +1,22 @@
 import asyncio
 
-
 class Calculater(AppBase):
     __version__ = "1.0.0"
     app_name = "calculater"  # this needs to match "name" in api.yaml
     def __init__(self, redis, logger, console_logger=None):
-
         super().__init__(redis, logger, console_logger)
 
     async def add(self,num1,num2):
-        return str(num1 + num2)
+        return str(num1st + num2)
+
     async def sub(self, num1, num2):
         return str(num1 - num2)
+
     async def mul(self,num1,num2):
         return str(num1 * num2)
+
     async def dev(self,num1,num2):
         return str(num1 / num2)
-
-
 
 
 if __name__ == "__main__":
