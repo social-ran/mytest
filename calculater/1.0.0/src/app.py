93c775852a531727d7495f7ded5b0e330fdda844
social-ran <68770158+social-ran@users.noreply.github.com>
2020-07-26 18:47:34 +0800

    Update app.py

--- calculater/1.0.0/src/app.py
+++ calculater/1.0.0/src/app.py
@@ -1,9 +1,9 @@
 import asyncio
 
 
-class HelloWorld(AppBase):
+class Calculater(AppBase):
     __version__ = "1.0.0"
-    app_name = "hello_world"  # this needs to match "name" in api.yaml
+    app_name = "calculater"  # this needs to match "name" in api.yaml
     def __init__(self, redis, logger, console_logger=None):
 
         super().__init__(redis, logger, console_logger)
@@ -21,4 +21,4 @@ class HelloWorld(AppBase):
 
 
 if __name__ == "__main__":
-    asyncio.run(HelloWorld.run(), debug=True)
+    asyncio.run(Calculater.run(), debug=True)
