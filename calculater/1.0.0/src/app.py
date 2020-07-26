4a5b6c1711c4b97bfcd1d5d0266fecd085b0a639
social-ran <68770158+social-ran@users.noreply.github.com>
2020-07-26 18:39:53 +0800

    Update app.py

--- calculater/1.0.0/src/app.py
+++ calculater/1.0.0/src/app.py
@@ -1,1 +1,24 @@
+import asyncio
 
+
+class HelloWorld(AppBase):
+    __version__ = "1.0.0"
+    app_name = "hello_world"  # this needs to match "name" in api.yaml
+    def __init__(self, redis, logger, console_logger=None):
+
+        super().__init__(redis, logger, console_logger)
+
+    async def add(self,num1,num2):
+        return str(num1 + num2)
+    async def sub(self, num1, num2):
+        return str(num1 - num2)
+    async def mul(self,num1,num2):
+        return str(num1 * num2)
+    async def dev(self,num1,num2):
+        return str(num1 / num2)
+
+
+
+
+if __name__ == "__main__":
+    asyncio.run(HelloWorld.run(), debug=True)
